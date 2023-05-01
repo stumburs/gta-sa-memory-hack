b14e5f4ff4e8582bc60da3002076489919fa2498
stumburs <stumburs@inbox.lv>
2023-05-01 20:19:04 +0300

    Fixed text not being displayed properly.

--- gta-sa-memory-hack/MemoryFunctions.cpp
+++ gta-sa-memory-hack/MemoryFunctions.cpp
@@ -1,6 +1,7 @@
 #include "MemoryFunctions.h"
 #include <string>
 #include "Addresses.h"
+#include <iostream>
 
 namespace MemoryFunctions
 {
@@ -12,14 +13,28 @@ namespace MemoryFunctions
 		p_handle = handle;
 	}
 
+	//void DisplayIngameAlert(const std::string& text)
+	//{
+	//	static int alert_count = 0; // Keep track of the number of alerts displayed
+	//	if (alert_count > 1)
+	//		alert_count = 0;
+	//	std::string unique_text = menu_top_bar[alert_count] + " " + text;
+	//	std::cout << unique_text << "\n";
+	//	SetValue(Addresses::text_box, unique_text.c_str());
+	//	alert_count++;
+	//}
 	void DisplayIngameAlert(const std::string& text)
 	{
 		static int alert_count = 0; // Keep track of the number of alerts displayed
 		if (alert_count > 1)
 			alert_count = 0;
 		std::string unique_text = menu_top_bar[alert_count] + " " + text;
+		std::cout << unique_text << "\n";
 		const char* alert_text = unique_text.c_str();
-		SetValue(Addresses::text_box, alert_text, strlen(alert_text) + 1);
+		size_t size = strlen(alert_text) + 1;
+		char buffer[150];
+		strncpy_s(buffer, alert_text, size);
+		SetValue(Addresses::text_box, buffer, size);
 		alert_count++;
 	}
 }
