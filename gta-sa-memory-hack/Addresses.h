c261199160be81663b24a420c7c123dc3c64baca
stumburs <stumburs@inbox.lv>
2023-05-01 22:21:35 +0300

    Extremely janky menu working.

--- gta-sa-memory-hack/Addresses.h
+++ gta-sa-memory-hack/Addresses.h
@@ -6,4 +6,6 @@ namespace Addresses
 	const LPCVOID money = reinterpret_cast<LPCVOID>(0xB7CE50);
 	const LPCVOID game_speed = reinterpret_cast<LPCVOID>(0xB7CB64);
 	const LPCVOID text_box = reinterpret_cast<LPCVOID>(0xBAA7A0);
+	const LPCVOID hesoyam_cheat = reinterpret_cast<LPCVOID>(0x969133);
+	const LPCVOID spawn_rhino = reinterpret_cast<LPCVOID>(0x969141);
 };
--- gta-sa-memory-hack/gta-sa-memory-hack.cpp
+++ gta-sa-memory-hack/gta-sa-memory-hack.cpp
@@ -10,16 +10,26 @@
 int timer = 0;
 
 using InputFunction = std::function<void()>;
-using InputMap = std::map<char, std::pair<bool, InputFunction>>;
+using InputMap = std::map<char, std::pair<int, InputFunction>>;
 
-InputMap CreateInputMap()
+
+enum MenuScreens
 {
-	InputMap input_map = {
-		{'1', std::make_pair(false, PlayerFunctions::AddMoney)},
-		{'2', std::make_pair(false, PlayerFunctions::SpeedUpGame)},
-		{'3', std::make_pair(false, PlayerFunctions::ResetGameSpeed)}
-	};
-	return input_map;
+	FIRST = 0,
+	PLAYER,
+	WORLD
+};
+
+int menu = MenuScreens::FIRST;
+
+void IncreaseMenuToPlayer()
+{
+	menu = MenuScreens::PLAYER;
+}
+
+void IncreaseMenuToWorld()
+{
+	menu = MenuScreens::WORLD;
 }
 
 void HandleInput(const InputMap& input_map)
@@ -29,17 +39,18 @@ void HandleInput(const InputMap& input_map)
 		{
 			if (!input.second.first)
 			{
-				const_cast<bool&>(input.second.first) = true;
+				const_cast<int&>(input.second.first) = true;
 				input.second.second();
 			}
 		}
 		else
-			const_cast<bool&>(input.second.first) = false;
+			const_cast<int&>(input.second.first) = false;
 }
 
 int main()
 {
 	bool running = true;
+
 
 	// Find game window
 	HWND hWnd = FindWindowA(0, "GTA: San Andreas");
@@ -55,10 +66,29 @@ int main()
 
 	bool is_menu_open = false;
 	bool is_alt_m_pressed = false;
-	std::string default_menu_text = "~g~1~s~ - $10000~n~~g~2~s~ - Speed up game~n~~g~3~s~ - Reset game speed~n~~g~ALT ~s~+ ~g~M ~s~to close";
-	std::string current_menu_text = default_menu_text;
+	std::string menu_first_screen = "~g~P~s~ - Player~n~~g~W~s~ - World~n~~g~ALT ~s~+ ~g~M ~s~to close";
+	std::string menu_player_screen = "Player~n~~g~1~s~ - $10000~n~~g~ALT ~s~+ ~g~M ~s~to close";
+	std::string menu_world_screen = "World~n~~g~1~s~ - Speed up game~n~~g~2~s~ - Reset game speed~n~~g~ALT ~s~+ ~g~M ~s~to close";
+	//std::string default_menu_text = "~g~1~s~ - $10000~n~~g~2~s~ - Speed up game~n~~g~3~s~ - Reset game speed~n~~g~4~s~ - Hesoyam~n~~g~ALT ~s~+ ~g~M ~s~to close";
+	std::string current_menu_text = menu_first_screen;
 
-	InputMap input_map = CreateInputMap();
+
+	/*
+	* CURRENT IMPLEMENTATION OF EVERYTHING HERE IS ABSOLUTELY AWFUL
+	*/
+	InputMap input_map_first = {
+		{'P', std::make_pair(0, IncreaseMenuToPlayer)},
+		{'W', std::make_pair(0, IncreaseMenuToWorld)},
+	};
+
+	InputMap input_map_player = {
+		{'1', std::make_pair(0, PlayerFunctions::AddMoney)},
+	};
+
+	InputMap input_map_world = {
+		{'1', std::make_pair(0, PlayerFunctions::SpeedUpGame)},
+		{'2', std::make_pair(0, PlayerFunctions::ResetGameSpeed)},
+	};
 
 	while (running)
 	{
@@ -81,15 +111,43 @@ int main()
 		if (is_menu_open)
 		{
 			// Get inputs
-			HandleInput(input_map);
+			switch (menu)
+			{
+			case MenuScreens::FIRST:
+				HandleInput(input_map_first);
+				break;
+			case MenuScreens::PLAYER:
+				HandleInput(input_map_player);
+				break;
+			case MenuScreens::WORLD:
+				HandleInput(input_map_world);
+				break;
+			default:
+				break;
+			}
 		}
+		else
+			menu = MenuScreens::FIRST;
 
 		std::cout << "Menu open: " << std::boolalpha << is_menu_open << "\n";
 
 		if (is_menu_open && timer == 0)
 		{
+			switch (menu)
+			{
+			case MenuScreens::FIRST:
+				current_menu_text = menu_first_screen;
+				break;
+			case MenuScreens::PLAYER:
+				current_menu_text = menu_player_screen;
+				break;
+			case MenuScreens::WORLD:
+				current_menu_text = menu_world_screen;
+				break;
+			default:
+				break;
+			}
 			MemoryFunctions::DisplayIngameAlert(current_menu_text);
-			current_menu_text = default_menu_text;
 		}
 
 		Sleep(50);
